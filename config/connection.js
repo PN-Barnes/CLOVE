b6c49a6f41446433e6bb1424bb8cc76ddf62fc0f
qtian13 <qiushuang.tian@gmail.com>
2021-08-04 02:25:38 -0700

    create connection to mysql

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+//heroku jawsdb
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // connect to local mysql
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize;
