8d5de6d0d8e9f23113afb241bffe63a9fe5944fd
qtian13 <qiushuang.tian@gmail.com>
2021-08-04 02:03:10 -0700

    add index and message model

---
+++ models/index.js
@@ -0,0 +1,76 @@
+const User = require('./User');
+const Product = require('./Product');
+const Rating = require('./Rating');
+const Message = require('./Message');
+const Basket = require('./Basket');
+
+User.hasMany(Product, {
+    foreignKey: 'user_id',
+});
+
+Product.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// foreign keys of basket
+Product.hasMany(Basket, {
+    foreignKey: 'product_id',
+});
+
+Basket.belongsTo(Product, {
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Basket, {
+    foreignKey: 'user_id',
+});
+
+Basket.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// foreign keys of rating
+User.hasMany(Rating, {
+    foreignKey: 'recipient_id',
+});
+
+Rating.belongsTo(User, {
+    foreignKey: 'recipient_id',
+});
+
+User.hasMany(Rating, {
+    foreignKey: 'poster_id',
+});
+
+Rating.belongsTo(User, {
+    foreignKey: 'poster_id',
+});
+
+Product.hasMany(Rating, {
+    foreignKey: 'product_id',
+});
+
+Rating.belongsTo(Product, {
+    foreignKey: 'product_id',
+});
+
+// foreign keys of message
+User.hasMany(Message, {
+    foreignKey: 'recipient_id',
+});
+
+Message.belongsTo(User, {
+    foreignKey: 'recipient_id',
+});
+
+User.hasMany(Message, {
+    foreignKey: 'sender_id',
+});
+
+Message.belongsTo(User, {
+    foreignKey: 'sender_id',
+});
+
+module.exports = { User, Product };
---
+++ models/message.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Message extends Model {};
+
+Message.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        recipient_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+                unique: false,
+            }
+        },
+        sender_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+                unique: false,
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'message',
+    }
+);
+
+module.exports = Message;
