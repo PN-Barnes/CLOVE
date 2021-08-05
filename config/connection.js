a70371eb71a1cbd19198cfa0ed0fc921916cd18a
Paul N Barnes <paulieb99@gmail.com>
2021-08-05 13:32:36 -0700

    fixed dependencies and starting seeding

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 
 //heroku jawsdb
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    // connect to local mysql
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
+  // connect to local mysql
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
 }
 
 module.exports = sequelize;
---
+++ seeds/index.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+
+const userData = require('./user-seeds');
+const ProductData = require('./product-seeds');
+const { User, Product } = require('../models');
+
+const seedDB = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  for (const product of ProductData) {
+    await Product.create({
+      ...product,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDB();
