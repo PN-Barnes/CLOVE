41dffe11ed60c37a96226be4e6b7a8e3a1d13c7d
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-04 17:39:30 -0700

    syncing database and seeding users and products

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,3 +4,21 @@ const productSeeds = require('./product-seeds');
 
 // loading sequilize model
 const sequelize = require('../config/connection');
+
+// arrow function to sync database and seed individual models
+const seedDB = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----');
+    
+    // seeding user data from user-seeds.js
+    await userSeeds();
+    console.log('\n----- USERS SEEDED -----\n');
+    // seeding product data from product-seeds.js
+    await productSeeds();
+    console.log('\n----- PRODUCTS SEEDED -----\n');
+
+    process.exit(0);
+};
+
+// calling seedDB
+seedDB();
