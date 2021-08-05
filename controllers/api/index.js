b42d91560cbdbd66a8a6886fc447c6cb403e4888
qtian13 <qiushuang.tian@gmail.com>
2021-08-05 15:06:33 -0700

    add rating API to index routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const basketRoutes = require('./basket-routes');
+const ratingRoutes = require('./rating-routes');
 
 router.use('/users', userRoutes);
 router.use('/baskets', basketRoutes);
+router.use('/ratings', ratingRoutes);
 
 module.exports = router;
