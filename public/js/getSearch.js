8b0a572ae285bc7996d71c38bb2d1e57580da296
Paul N Barnes <paulieb99@gmail.com>
2021-08-06 14:02:47 -0700

    added files for search function INCOMPLETE

---
+++ public/js/getSearch.js
@@ -0,0 +1,22 @@
+const getSearchResults = async (event) => {
+  event.preventDefault();
+
+  const zipcode = document.querySelector('#zipCode').value.trim();
+  const username = document.querySelector('#usernameInput').value.trim();
+  const product_name = document.querySelector('#productInput').value.trim();
+
+  const response = await fetch(`/api/baskets/${product_name}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.locaiton.replace('listing');
+  } else {
+    alert('Failed to search for Product.');
+  }
+};
+
+document.querySelector('form').addEventListener('submit', getSearchResults);
