aacab1f7b710dca6e1e51df55cc80cefdafd0710
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-05 17:35:30 -0700

    loading in seed constants for ratings, messages, and baskets

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,21 @@
-const sequelize = require("../config/connection");
-
-const userData = require("./userData");
-const ProductData = require("./productData");
-const { User, Product } = require("../models");
+const seedUsers = require('./user-seeds');
+const seedProducts = require('./product-seeds');
+const seedRatings = require('./rating-seeds');
+const seedMessages = require('./message-seeds');
+const seedBasket = require('./basket-seeds');
+const sequelize = require('../config/connection');
 
 const seedDB = async () => {
   await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-  for (const product of ProductData) {
-    await Product.create({
-      ...product,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
+  await seedProducts();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
 
   process.exit(0);
 };
 
 seedDB();
-
-// USE THIS FILE TO SEED DATABASE
