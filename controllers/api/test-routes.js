7b9a8aabc6cc7d6731eadb4be9494650b842255a
qtian13 <qiushuang.tian@gmail.com>
2021-08-07 15:06:24 -0700

    file converted to base64string and can be uploded to cloudinary successfully

--- controllers/api/test-routes.js
+++ controllers/api/test-routes.js
@@ -3,26 +3,26 @@ const cloudinary = require('../../config/cloudinary');
 
 router.post("/", async (req, res) => {
   try {
-    const url = "https://images.unsplash.com/photo-1544526226-d4568090ffb8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80";
-    // const url = "./public/assets/profile.jpeg"
-    const uploadedResponse = await cloudinary.uploader
-                                      .upload(url, {
-                                        "resource_type": "image",
-                                        upload_preset: 'dev_setup'
-                                      });
-    
-    console.log("upload successfully");
-    console.log(uploadedResponse);
-    // res.status(200).json({message: "ayayayayay"});
-    res.status(200).json(uploadedResponse);
-    
-    // const fileStr = req.body.data;
+    // const url = "https://images.unsplash.com/photo-1544526226-d4568090ffb8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80";
+    // // const url = "./public/assets/profile.jpeg"
     // const uploadedResponse = await cloudinary.uploader
-    //                                   .upload(fileStr, {
+    //                                   .upload(url, {
+    //                                     "resource_type": "image",
     //                                     upload_preset: 'dev_setup'
     //                                   });
-    // console.log("success", JSON.stringify(response, null, 2));
-    // res.status(200).json("ayayayayay");
+    
+    // console.log("upload successfully");
+    // console.log(uploadedResponse);
+    // // res.status(200).json({message: "ayayayayay"});
+    // res.status(200).json(uploadedResponse);
+    
+    const fileStr = req.body.data;
+    const uploadedResponse = await cloudinary.uploader
+                                      .upload(fileStr, {
+                                        upload_preset: 'dev_setup'
+                                      });
+    console.log("success", JSON.stringify(uploadedResponse, null, 2));
+    res.status(200).json("ayayayayay");
     
   } catch (err) {
     console.log(err);
--- public/js/test.js
+++ public/js/test.js
@@ -1,39 +1,71 @@
-const uploadHandler = async () => {
-  const response = await fetch("/api/tests", {
-    method: 'POST',
-    body: JSON.stringify({
-      file: "/assets/profile"
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
 
-  if (response.ok) {
-    console.log("uploaded");
-  } else {
-    alert("fail to upload");
-  }
-
-}
-
-const handleFiles = (e) => {
+let imageData;
+const handleFiles = async (e) => {
+  console.log("file loaded");
   const file = e.target.files[0];
-  previewFile(file);
+  // check if file is loaded correctly
+  // previewFile(file);
+  // const dataUrl = getDataUrl(file);
+  // console.log(dataUrl);
+  imageData = await getBase64(file);
 }
 
-const previewFile = file => {
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onloadend = () => {
-    setPreviewSource(reader.result);
+// function getDataUrl(img) {
+//   // Create canvas
+//   const canvas = document.createElement('canvas');
+//   const ctx = canvas.getContext('2d');
+//   // Set width and height
+//   canvas.width = img.width;
+//   canvas.height = img.height;
+//   // Draw the image
+//   ctx.drawImage(img, 0, 0);
+//   return canvas.toDataURL('image/jpeg');
+// }
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
+
+const submitFileHandler = async (e) => {
+  e.preventDefault();
+  console.log("submit file");
+  if (!imageData) return;
+  uploadeImage(imageData);    // react thing
+}
+
+const uploadeImage = async (base64EncodedImage) => {
+  console.log(base64EncodedImage);
+  try {
+    const response = await fetch("/api/tests", {
+      method: 'POST',
+      body: JSON.stringify({
+        data: base64EncodedImage
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    
+    if (response.ok) {
+      console.log("uploaded");
+    } else {
+      alert("fail to upload");
+    }
+
+  } catch (err) {
+    console.log(err);
   }
 }
 
-const inputElement = document
-  .querySelector('#avatar');
+// file input element
+const inputElement = document.querySelector('#avatar');
 inputElement.addEventListener('change', handleFiles);
 
+// upload button
 document
   .querySelector('form')
   .addEventListener('submit', submitFileHandler);
