70a5abb1f0c8f734b31e714d3a4c05fe32c648f1
qtian13 <qiushuang.tian@gmail.com>
2021-08-07 16:41:00 -0700

    format the file correctly

--- controllers/api/message-routes.js
+++ controllers/api/message-routes.js
@@ -1,150 +1,153 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const { Op } = require("sequelize");
-const { User, Basket, Product, Rating, Message } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { User, Basket, Product, Rating, Message } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // GET all message
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const dbMessageData = await Message.findAll({
-            where: {
-                [Op.or]: [
-                    { recipient_id: req.session.userId },
-                    { sender_id: req.session.userId }
-                ]
-            },
-            include: [
-                {
-                    model: User,
-                    attributes: {
-                        exclude: ['password'],
-                    },
-                    as: "sender"
-                },
-                {
-                    model: User,
-                    attributes: {
-                        exclude: ['password'],
-                    },
-                    as: "recipient"
-                }
-            ]
-        });
-        res.status(200).json(dbMessageData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const dbMessageData = await Message.findAll({
+      where: {
+        [Op.or]: [
+          { recipient_id: req.session.userId },
+          { sender_id: req.session.userId },
+        ],
+      },
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ["password"],
+          },
+          as: "sender",
+        },
+        {
+          model: User,
+          attributes: {
+            exclude: ["password"],
+          },
+          as: "recipient",
+        },
+      ],
+    });
+    res.status(200).json(dbMessageData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // GET message with id === req.params.id
-router.get('/:id', withAuth, async (req, res) => {
-    try {
-        const dbMessageData = await Message.findByPk(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: {
-                        exclude: ['password'],
-                    },
-                    as: "sender"
-                },
-                {
-                    model: User,
-                    attributes: {
-                        exclude: ['password'],
-                    },
-                    as: "recipient"
-                }
-            ]
-        });
-        res.status(200).json(dbMessageData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+router.get("/:id", withAuth, async (req, res) => {
+  try {
+    const dbMessageData = await Message.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ["password"],
+          },
+          as: "sender",
+        },
+        {
+          model: User,
+          attributes: {
+            exclude: ["password"],
+          },
+          as: "recipient",
+        },
+      ],
+    });
+    res.status(200).json(dbMessageData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-// GET info of message of certain user with 
+// GET info of message of certain user with
 //recipient_id === req.params.id && sender_id: req.session.userId
 // || sender_id === req.params.id && recipient_id: req.session.userId
-router.get('/with/:id', withAuth, async (req, res) => {
-    try {
-        const dbMessageData = await Message.findAll({
-            where: {
-                [Op.or]: {
-                    [Op.and]: [
-                        { recipient_id: req.session.userId },
-                        { sender_id: req.params.id }
-                    ], 
-                    [Op.and]: [
-                        { recipient_id: req.params.id },
-                        { sender_id: req.session.userId }
-                    ]
-                },
-            },
-            include: [
-                {
-                    model: User,
-                    attributes: {
-                        exclude: ['password'],
-                    },
-                    as: "sender"
-                },
-                {
-                    model: User,
-                    attributes: {
-                        exclude: ['password'],
-                    },
-                    as: "recipient"
-                }
-            ]
-        });
-        res.status(200).json(dbMessageData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+router.get("/with/:id", withAuth, async (req, res) => {
+  try {
+    const dbMessageData = await Message.findAll({
+      where: {
+        [Op.or]: {
+          [Op.and]: [
+            { recipient_id: req.session.userId },
+            { sender_id: req.params.id },
+          ],
+          [Op.and]: [
+            { recipient_id: req.params.id },
+            { sender_id: req.session.userId },
+          ],
+        },
+      },
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ["password"],
+          },
+          as: "sender",
+        },
+        {
+          model: User,
+          attributes: {
+            exclude: ["password"],
+          },
+          as: "recipient",
+        },
+      ],
+    });
+    res.status(200).json(dbMessageData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // CREATE a new message
-router.post('/', withAuth, async (req, res) => {
-    try {
-        // assume user can provide recipient_id
-        const dbMessageData = await Message.create({
-            // req.body
-            content: req.body.content,
-            recipient_id: req.body.recipient_id, // need to update with User.find
-            sender_id: req.session.userId
-        });
-        res.status(200).json(dbMessageData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+router.post("/", withAuth, async (req, res) => {
+  try {
+    // assume user can provide recipient_id
+    const dbMessageData = await Message.create({
+      // req.body
+      content: req.body.content,
+      recipient_id: req.body.recipient_id, // need to update with User.find
+      sender_id: req.session.userId,
+    });
+    res.status(200).json(dbMessageData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // DELETE an existing message with id === req.params.id
-router.delete('/:id', withAuth, async (req, res) => {
-    try {
-        let dbMessageData = await Message.findByPk(req.params.id);
-        if (!dbMessageData) {
-            res.status(400).json({ message: 'No message found with that id!' });
-            return;
-        } else if (dbMessageData.get({plain: true}).sender_id !== req.session.userId) {
-            res.status(400).json({ message: 'Sorry you cannot delete the message with that id!' });
-        } else {
-            dbMessageData = await Message.destroy(
-                {
-                where: {
-                    id: req.params.id,
-                },
-            });
-            res.status(200).json({ message: 'Delete the message successfully!' });
-        }
-    } catch (err) {
-        res.status(500).json(err);
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    let dbMessageData = await Message.findByPk(req.params.id);
+    if (!dbMessageData) {
+      res.status(400).json({ message: "No message found with that id!" });
+      return;
+    } else if (
+      dbMessageData.get({ plain: true }).sender_id !== req.session.userId
+    ) {
+      res
+        .status(400)
+        .json({ message: "Sorry you cannot delete the message with that id!" });
+    } else {
+      dbMessageData = await Message.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json({ message: "Delete the message successfully!" });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,129 +1,136 @@
-const router = require('express').Router();
-const { User } = require('../../models');
-const cloudinary = require('../../config/cloudinary');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { User } = require("../../models");
+const cloudinary = require("../../config/cloudinary");
+const withAuth = require("../../utils/auth");
 
 // GET user info with username === req.params.username
-router.get('/name/:username', async (req, res) => {
-    try {
-        const dbUserData = await User.findOne({
-            attributes: {
-                exclude: ['password']
-            },
-            where: { username: req.params.username }
-        });
+router.get("/name/:username", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      attributes: {
+        exclude: ["password"],
+      },
+      where: { username: req.params.username },
+    });
 
-        if (!dbUserData) {
-            res.status(400).json({ message: "No user found with this username!"});
-        } else {
-            const user = dbUserData.get({ plain: true });
-            res.status(200).json(user);
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user found with this username!" });
+    } else {
+      const user = dbUserData.get({ plain: true });
+      res.status(200).json(user);
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // CREATE a new user
-router.post('/', async (req, res) => {
-    try {
-        const dbUserData = await User.create(req.body);
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create(req.body);
 
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            req.session.userId = dbUserData.get({ plain: true }).id;
-            
-            res.status(200).json(dbUserData);
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.userId = dbUserData.get({ plain: true }).id;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // change profile image of user logged in
 router.post("/profile", withAuth, async (req, res) => {
   try {
     const fileStr = req.body.data;
-    const uploadedResponse = await cloudinary.uploader
-                                      .upload(fileStr, {
-                                        upload_preset: 'dev_setup'
-                                      });
+    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "dev_setup",
+    });
     console.log("success", JSON.stringify(uploadedResponse, null, 2));
 
     let dbUserData = await User.findByPk(req.session.userId);
     if (!dbUserData) {
-        res.status(400).json({ message: 'No user found with that id!' });
-        return;
-    } else if (dbUserData.get({plain: true}).id !== req.session.userId) {
-        res.status(400).json({ message: 'Sorry you cannot edit the profile of that user!' });
+      res.status(400).json({ message: "No user found with that id!" });
+      return;
+    } else if (dbUserData.get({ plain: true }).id !== req.session.userId) {
+      res
+        .status(400)
+        .json({ message: "Sorry you cannot edit the profile of that user!" });
     } else {
-        dbUserData = await User.update(
-            {
-                profile_picture: uploadedResponse.url,
-            },
-            {
-                where: {
-                    id: req.session.userId,
-                },
-            }
-        );
-        console.log(dbUserData);
-        res.status(200).json({ message: 'Updated successfully!' });
+      dbUserData = await User.update(
+        {
+          profile_picture: uploadedResponse.url,
+        },
+        {
+          where: {
+            id: req.session.userId,
+          },
+        }
+      );
+      console.log(dbUserData);
+      res.status(200).json({ message: "Updated successfully!" });
     }
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
-})
+});
 
 module.exports = router;
 
 // login to an existing account
-router.post('/login', async (req, res) => {
-    try {
-        const dbUserData = await User.findOne({
-            where: {
-                username: req.body.username,
-            },
-        });
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
 
-        // if username is not found
-        if (!dbUserData) {
-            res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
-            return;
-        }
-
-        const validPassword = await dbUserData.checkPassword(req.body.password);
-
-        // if username and password do not match
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            req.session.userId = dbUserData.get({ plain: true }).id;
-
-            res.status(200).json({ user: dbUserData, message: 'You are now logged in!' });
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+    // if username is not found
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password. Please try again!" });
+      return;
     }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    // if username and password do not match
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password. Please try again!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.userId = dbUserData.get({ plain: true }).id;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // logout the current account and destroy the session
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
