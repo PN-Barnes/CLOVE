1ae4957b9183e926fad547f4a3f94de94f1d511f
Paul N Barnes <paulieb99@gmail.com>
2021-08-07 16:53:33 -0700

    not working code

--- public/js/getSearch.js
+++ public/js/getSearch.js
@@ -5,19 +5,17 @@ const getSearchResults = async (event) => {
   const zipcode = document.querySelector('#zipCode').value.trim();
   const username = document.querySelector('#usernameInput').value.trim();
   const product_name = document.querySelector('#productInput').value.trim();
-  if (product_name) {
-    const response = await fetch(`/api/baskets/product/${product_name}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.ok) {
-      console.log('made it through the backend');
-      document.location.replace('/listing/searchResults');
-    } else {
-      alert('Failed to search for Product.');
-    }
+  const response = await fetch(`/api/baskets`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    console.log('made it through the backend');
+    document.location.replace('/listing/searchResults');
+  } else {
+    alert('Failed to search for Product.');
   }
 };
 
