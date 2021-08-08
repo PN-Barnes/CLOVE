af527de7446626270e06d05d6a22c5cd89ae7869
qtian13 <qiushuang.tian@gmail.com>
2021-08-07 19:04:27 -0700

    debug heroku deploy

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,9 +2,12 @@ const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  // cloud_name: process.env.CLOUDINARY_NAME,
+  // api_key: process.env.CLOUDINARY_API_KEY,
+  // api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name:dpq37t5jn,
+  api_key:767364295588399,
+  api_secret:XyNE9Q1JlZxhUtevGEU458VUUOg
 })
 
 module.exports = cloudinary;
