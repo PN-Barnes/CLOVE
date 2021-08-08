ad16cdfab89394c5796a123788d8e095b3007eb5
qtian13 <qiushuang.tian@gmail.com>
2021-08-08 00:51:31 -0700

    user can search by zipcode/username/product name

--- public/js/getSearch.js
+++ public/js/getSearch.js
@@ -1,22 +1,59 @@
-const getSearchResults = async (event) => {
-  event.preventDefault();
+// const getSearchResults = async (event) => {
+//   event.preventDefault();
 
-  console.log('this button was pressed!');
-  const zipcode = document.querySelector('#zipCode').value.trim();
-  const username = document.querySelector('#usernameInput').value.trim();
-  const product_name = document.querySelector('#productInput').value.trim();
-  const response = await fetch(`/api/baskets/product/${product_name}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  if (response.ok) {
-    console.log('made it through the backend');
+//   console.log('this button was pressed!');
+//   const zipcode = document.querySelector('#zipCode').value.trim();
+//   const username = document.querySelector('#usernameInput').value.trim();
+//   const product_name = document.querySelector('#productInput').value.trim();
+//   const response = await fetch(`/api/baskets/product/${product_name}`, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+//   if (response.ok) {
+//     console.log('made it through the backend');
+//   } else {
+//     alert('Failed to search for Product.');
+//   }
+// };
+// document
+//   .querySelector('#searchButton')
+//   .addEventListener('click', getSearchResults);
+const searchHandler = async (event) => {
+  event.preventDefault();
+  const term = document.querySelector('#term').value;
+  // const searchBy = document.querySelector('#search-by option:checked').value;
+  var sel = document.getElementById("search-by");
+var searchBy= sel.options[sel.selectedIndex].text;
+
+  if (term) {
+    if (searchBy === "Username") {
+      console.log("search by username");
+    } else if (searchBy === "Zip Code") {
+      console.log("search by zip code");
+    } else {
+      console.log("search by product name");
+    }
+
+      // const response = await fetch(`/api/baskets`, {
+      //     method: 'POST',
+      //     body: JSON.stringify({ stock, price, description, product_id }),
+      //     headers: {
+      //         'Content-Type': 'application/json',
+      //     },
+      // });
+  
+      // redirect to dashboard page after creating a new blog successfully
+      // if (response.ok) {
+      //     document.location.replace('/profile');
   } else {
-    alert('Failed to search for Product.');
+    alert('Please enter a string!');
   }
 };
+
+
+
 document
-  .querySelector('#searchButton')
-  .addEventListener('click', getSearchResults);
+  .querySelector('form')
+  .addEventListener('submit', searchHandler);
