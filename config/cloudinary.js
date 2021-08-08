6d898555f4e53ee8a0e7dae05ed5c240ffee116c
qtian13 <qiushuang.tian@gmail.com>
2021-08-07 20:28:56 -0700

    cloudinary works on heroku

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,11 @@
 const cloudinary = require('cloudinary');
-require('dotenv').config();
+// require('dotenv').config();
 
-if (!process.env.CLOUDINARY_URL) {
-  cloudinary.vs.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
+// if (!process.env.CLOUDINARY_URL) {
+//   cloudinary.vs.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// })
 
 module.exports = cloudinary;
