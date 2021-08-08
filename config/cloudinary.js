fdd7f8ed09e141ce257de90adcbdcdfa2a8cc415
qtian13 <qiushuang.tian@gmail.com>
2021-08-07 19:50:01 -0700

    try using cloudinary credentials auto-generated by heroku

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,13 +1,13 @@
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
+const cloudinary = require('cloudinary');
+//require('dotenv').config();
 
-cloudinary.config({
+//cloudinary.config({
   // cloud_name: process.env.CLOUDINARY_NAME,
   // api_key: process.env.CLOUDINARY_API_KEY,
   // api_secret: process.env.CLOUDINARY_API_SECRET,
-  cloud_name:dpq37t5jn,
-  api_key:767364295588399,
-  api_secret:XyNE9Q1JlZxhUtevGEU458VUUOg
-})
+//  cloud_name:'dpq37t5jn',
+//  api_key:'767364295588399',
+//  api_secret:'XyNE9Q1JlZxhUtevGEU458VUUOg',
+//})
 
 module.exports = cloudinary;
